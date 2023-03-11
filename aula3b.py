e34ea20b868ab9b31cffb41b8f913f8be32794cc
Amâncio Zangui <114434987+Amanciozangui@users.noreply.github.com>
2023-03-11 02:07:08 +0100

    Add files via upload

---
+++ aula3b.py
@@ -0,0 +1,8 @@
+n1 = int (input("Escreve um Valor: "))
+n2 = int (input ("Escreve outro Valor: "))
+s = n1+n2
+m = n1*n2
+d = n1/n2
+di = n1//n2
+e = n1**n2
+print ("A Soma é {}, a Multiplicação é {} e a divisão é {:.0f}".format (s,m,d))
